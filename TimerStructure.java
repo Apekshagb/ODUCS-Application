7320bf9d546c47a44ca66a07713a8eb34071ea0b
Apekshagb <apeksha.gb@gmail.com>
2015-03-02 14:31:24 -0500

    Create TimerStructure.java

---
+++ TimerStructure.java
@@ -0,0 +1,19 @@
+package com.example.oducs_mobile;
+
+public class TimeStructure {
+
+	private String shake_time;
+
+	public String getShake_time() {
+		return shake_time;
+	}
+
+	public void setShake_time(String shake_time) {
+		this.shake_time = shake_time;
+	}
+
+	public String toString() {
+		return ("Shake time =" + shake_time);
+	}
+
+}

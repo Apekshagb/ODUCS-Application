1d05d97db7ca16d23637770b4319bfa6b9437131
Apekshagb <apeksha.gb@gmail.com>
2015-03-02 13:52:04 -0500

    Create About.java

---
+++ About.java
@@ -0,0 +1,22 @@
+package com.example.oducs_mobile;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class About extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+		
+	}
+
+}
